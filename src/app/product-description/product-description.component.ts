b98336bb49b0cc780f03e6359aec657f9da28bf8
djaniell <danielepluribus@gmail.com>
2019-11-05 01:32:01 +0100

    step3

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import {ProductService} from '../product.service';
+
 @Component({
   selector: 'app-product-description',
   templateUrl: './product-description.component.html',
@@ -7,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDescriptionComponent implements OnInit {
 
-  constructor() { }
+  albumInfo;
+  _productService: any;
+  
+  constructor(private_productService: productService) { }
 
   ngOnInit() {
+    this._productService.getAlbum(1).subscribe(response =>
+      this.albumInfo = response);
   }
 
 }
---
+++ src/app/product.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+
+import { Http, Response } from '@angular/http';
+
+import 'rxjs/add/operator/map'
+
+@Injectable()
+export class ProductService {
+
+  private_albumUrl = '../assets/album.json';
+ 
+  _http: any;
+
+  constructor(private_http: Http) { }
+
+  getAlbum(id: number) {
+    return this._http.get(this._albumUrl).map((response)  =>
+    response.json());
+  }
+  _albumUrl(_albumUrl: any) {
+    throw new Error("Method not implemented.");
+  }
+
+}
